dfe43751db5c8736f7c839bc9b7dc469eec6b406
codewithnimraa <136242525+codewithnimraa@users.noreply.github.com>
2023-08-26 17:21:40 +0500

    Add files via upload

---
+++ assigment_1.dart
@@ -0,0 +1,18 @@
+import 'package:firstproject/wpchatscr.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const wpchatscr());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: wpchatscr(),
+    );
+  }
+}
